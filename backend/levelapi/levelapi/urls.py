e9a0c6f3ecbeb97bd31d280d49295d201f1e04d6
Aadil Shah <adilns786@gmail.com>
2025-01-19 03:58:38 +0530

    app review working

--- backend/levelapi/levelapi/urls.py
+++ backend/levelapi/levelapi/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import get_company_and_play_store_details
+from .views import get_company_and_play_store_details,get_reviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scrape-data/', get_company_and_play_store_details, name='scrape_data'),
+    path('scrape-review/', get_reviews, name='get_reviews'),
+
 ]
